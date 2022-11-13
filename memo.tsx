cefc0b002c172a5e3be02cc4f808d8a187370ff4
shu kajitani <hiiragi.univ@gmail.com>
2022-11-13 11:22:51 +0900

    first commit

---
+++ memo.tsx
@@ -0,0 +1,61 @@
+export function EditSubtitles() {
+  const form = useForm({
+    initialValues: {
+      employees: [
+        { name: 'John Doe', email: 'john@mantine.dev' },
+        { name: 'Bill Love', email: 'bill@mantine.dev' },
+        { name: 'Nancy Eagle', email: 'nanacy@mantine.dev' },
+        { name: 'Lim Notch', email: 'lim@mantine.dev' },
+        { name: 'Susan Seven', email: 'susan@mantine.dev' },
+      ],
+    },
+  });
+
+  const fields = form.values.employees.map((_, index) => (
+    <Draggable key={index} index={index} draggableId={index.toString()}>
+      {(provided: any) => (
+        <Group ref={provided.innerRef} mt="xs" {...provided.draggableProps}>
+          <Center {...provided.dragHandleProps}>
+            <IconGripVertical size={18} />
+          </Center>
+          <TextInput placeholder="John Doe" {...form.getInputProps(`employees.${index}.name`)} />
+          <TextInput
+            placeholder="example@mail.com"
+            {...form.getInputProps(`employees.${index}.email`)}
+          />
+        </Group>
+      )}
+    </Draggable>
+  ));
+
+  return (
+    <Box sx={{ maxWidth: 500 }} mx="auto">
+      <DragDropContext
+        onDragEnd={({ destination, source } :any) =>
+          form.reorderListItem('employees', { from: source.index, to: destination.index })
+        }
+      >
+        <Droppable droppableId="dnd-list" direction="vertical">
+          {(provided: any) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {fields}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+      
+
+      <Group position="center" mt="md">
+        <Button onClick={() => form.insertListItem('employees', { name: '', email: '' })}>
+          Add employee
+        </Button>
+      </Group>
+
+      <Text size="sm" weight={500} mt="md">
+        Form values:
+      </Text>
+      <Code block>{JSON.stringify(form.values, null, 2)}</Code>
+    </Box>
+  );
+}
